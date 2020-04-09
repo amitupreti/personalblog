b1bce02b5462dd6d01e4d5b264cbff0f8c2efcf2
Amit Upreti <a.u.aua937@gmail.com>
2020-04-09 17:08:40 +0545

    Update contact.js

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -43,7 +43,7 @@ export default class Contact extends React.Component {
                   </p>
                   <input type="hidden" name="form-name" value="contactForm" />
                       
-      <div class="g-recaptcha" data-sitekey="6Lc-H-gUAAAAAO_AMaS4yJgRvcuGvYA4c69YHnr8"></div>
+                  <div data-netlify-recaptcha="true" ></div>
                   <p className="form-row">
                       
                     <button type="submit" className="button">Send Message</button>
